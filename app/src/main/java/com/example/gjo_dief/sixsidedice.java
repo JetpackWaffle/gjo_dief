da786326bb8189a41c76e4c776cbb0c780c681d6
JetpackWaffle <awesomedude2011gray@gmail.com>
2022-09-26 18:55:23 -0500

    added shaking and sound functionality

--- app/src/main/java/com/example/gjo_dief/sixsidedice.java
+++ app/src/main/java/com/example/gjo_dief/sixsidedice.java
@@ -2,17 +2,30 @@ package com.example.gjo_dief;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class sixsidedice extends AppCompatActivity {
 
     private ImageView imageViewDice;
     private Random rng = new Random();
+
+    //shake things
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +39,56 @@ public class sixsidedice extends AppCompatActivity {
                 rollD6();
             }
         });
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12)
+            {
+                rollD6();
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
     }
 
     private void rollD6()
     {
         int randomNumber = rng.nextInt(6) + 1;
-
+        MediaPlayer one= MediaPlayer.create(sixsidedice.this,R.raw.dominated);
+        MediaPlayer crit= MediaPlayer.create(sixsidedice.this,R.raw.domination);
         switch(randomNumber)
         {
             case 1:
                 imageViewDice.setImageResource(R.drawable.d61);
                 Toast.makeText(this, "Oooh, unfortunate, a 1.", Toast.LENGTH_LONG).show();
+                one.start();
                 break;
             case 2:
                 imageViewDice.setImageResource(R.drawable.d62);
@@ -52,7 +104,8 @@ public class sixsidedice extends AppCompatActivity {
                 break;
             case 6:
                 imageViewDice.setImageResource(R.drawable.d66);
-                Toast.makeText(this, "YOOOOOO LET'S GOOOO", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "YOOOOOO A CRIT", Toast.LENGTH_LONG).show();
+                crit.start();
                 break;
         }
     }
